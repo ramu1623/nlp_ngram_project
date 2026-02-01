db0a4f6f9c1d89addcf1a3fb4b4deab56cd51168
d devi sri prasad <ramudsp23@gmail.com>
2026-02-01 19:21:09 +0530

    Updated frontend API URL for Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const generateNgrams = async (ngramType) => {
     setType(ngramType);
 
-    const res = await fetch("http://127.0.0.1:5000/ngrams", {
+    const res = await fetch("https://nlp-ngram-project.onrender.com/ngrams", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text })
